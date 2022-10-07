36a84caf045f1be5591bc6400b89b666d2baeea5
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-07 08:09:17 +0300

    commit tree c12dca24d818f65e357f5f0d10b24c12a605e341

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,20 @@
+class RecipesController < ApplicationController
+    def index 
+        user = User.find(session[:user_id])
+        recipes = user.recipes
+        render json: recipes
+    end
+
+    def create 
+        user = User.find_by(id: session[:user_id])
+        recipe = user.recipes.create!(recipe_params)
+        render json: recipe, status: :created 
+    end
+
+    private 
+
+    def recipe_params 
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
+
